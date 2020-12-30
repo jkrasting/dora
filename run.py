057ba3a9cdc95575ca76a0ffb7a64ad5bef01870
John Krasting <John.Krasting@noaa.gov>
2020-12-29 20:05:26 -0500

    Set run.py to be a standalone executable script

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python
+
 # -*- encoding: utf-8 -*-
 """
 Copyright (c) 2019 - present AppSeed.us
 """
 
 from app import app
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=5000,debug=True)
