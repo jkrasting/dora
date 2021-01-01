b304512159a6acb3a8580e483540fbac8ce8364e
John Krasting <John.Krasting@noaa.gov>
2021-01-01 13:24:34 -0500

    Broke scalar diagnostics into its own route

--- app/views.py
+++ app/views.py
@@ -26,62 +26,79 @@ def stream_template(template_name, **context):
 @app.route('/', defaults={'path': 'index.html'}, methods=["GET"])
 @app.route('/<path>', methods=['GET'])
 def index(path):
-
     try:
-        if "blank" in path:
-
-            import base64
-            import io
-            import gfdlvitals
-            import matplotlib.pyplot as plt
-
-            plt.switch_backend("Agg")
-
-            idnum = request.args.getlist("id")
-            idnum = None if len(idnum) == 0 else idnum
-
-            region = request.args.get("region") 
-            realm = request.args.get("realm")
-            smooth = request.args.get("smooth")
-            nyears = request.args.get("nyears")
-            trend = request.args.get("trend")
-            align = request.args.get("align")
-
-            trend = True if trend is not None else False
-            align = True if align is not None else False
-
-            smooth = None if (smooth == "" or smooth is None) else int(smooth)
-            nyears = None if (nyears == "" or nyears is None) else int(nyears)
-
-            if (region is None) or (realm is None):
-                return render_template( "login.html" )
-
-            fname = f"/Users/krasting/dbverif5/new/{region}Ave{realm}.db" 
-
-            if os.path.exists(fname):
-                dset = gfdlvitals.open_db(fname)
-            else:
-                msg = f"Unable to load SQLite file: {fname}"
-                return render_template('page-500.html',msg=msg)
-
-            def plot_gen():
-                for x in sorted(list(dset.columns)):
-                    fig = gfdlvitals.plot_timeseries(dset,trend=trend,smooth=smooth,var=x,\
-                          nyears=nyears,align_times=align,labels="Test")
-                    fig = fig[0]
-                    imgbuf = io.BytesIO()
-                    fig.savefig(imgbuf, format="png", bbox_inches="tight",dpi=72)
-                    plt.close(fig)
-                    imgbuf.seek(0)
-                    uri = 'data:image/png;base64,' + base64.b64encode(imgbuf.getvalue()).decode('utf-8').replace('\n', '')
-                    yield uri
-
-            content = {"rows":plot_gen(),"region":region.capitalize(),"realm":realm.capitalize()}
-            return Response(stream_template( path, **content ))
-
-        else:
-            # Serve the file (if exists) from app/templates/FILE.html
-            return render_template( path )
+        # Serve the file (if exists) from app/templates/FILE.html
+        return render_template( path )
     
     except TemplateNotFound:
         return render_template('page-404.html'), 404
+
+
+@app.route("/scalar-diags.html")
+def scalardiags():
+
+    idnum = request.args.getlist("id")
+    idnum = None if len(idnum) == 0 else idnum
+
+    region = request.args.get("region") 
+            region = request.args.get("region") 
+    region = request.args.get("region") 
+    realm = request.args.get("realm")
+    smooth = request.args.get("smooth")
+    nyears = request.args.get("nyears")
+    trend = request.args.get("trend")
+    align = request.args.get("align")
+
+    trend = True if trend is not None else False
+    align = True if align is not None else False
+
+    smooth = None if (smooth == "" or smooth is None) else int(smooth)
+    nyears = None if (nyears == "" or nyears is None) else int(nyears)
+
+    if (region is None) or (realm is None):
+        return render_template( "scalar-menu.html" )
+
+    fname = f"/Users/krasting/dbverif5/new/{region}Ave{realm}.db" 
+            fname = f"/Users/krasting/dbverif5/new/{region}Ave{realm}.db" 
+    fname = f"/Users/krasting/dbverif5/new/{region}Ave{realm}.db" 
+
+    if os.path.exists(fname):
+        dset = gfdlvitals.open_db(fname)
+    else:
+        msg = f"Unable to load SQLite file: {fname}"
+        return render_template('page-500.html',msg=msg)
+
+    def plot_gen():
+        for x in sorted(list(dset.columns)):
+            fig = gfdlvitals.plot_timeseries(dset,trend=trend,smooth=smooth,var=x,\
+                  nyears=nyears,align_times=align,labels="Test")
+            fig = fig[0]
+            imgbuf = io.BytesIO()
+            fig.savefig(imgbuf, format="png", bbox_inches="tight",dpi=72)
+            plt.close(fig)
+            imgbuf.seek(0)
+            uri = 'data:image/png;base64,' + base64.b64encode(imgbuf.getvalue()).decode('utf-8').replace('\n', '')
+            yield uri
+
+    content = {"rows":plot_gen(),"region":region.capitalize(),"realm":realm.capitalize()}
+    return Response(stream_template("scalar-diags.html", **content ))
+
+
+# Some sample database code below
+#    conn = pymysql.connect(host=dbhost,
+#                           port=dbport,
+#                           user="mdtadmin",
+#                           password="adminpassword",
+#                           db="mdt_tracker",
+#                           cursorclass=pymysql.cursors.DictCursor)
+#
+#    cursor = conn.cursor()
+#    sql = "SELECT * from master where id=890;"
+#    _ = cursor.execute(sql)
+#    result = cursor.fetchone()
+#
+#    print(sql)
+#    print(result)
+#
+#    cursor.close()
+#    conn.close()
