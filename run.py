487a2db561cd68e6fb31db225fc1aed53759e7a2
John Krasting <John.Krasting@noaa.gov>
2021-01-01 23:07:08 -0500

    SSL needed for google auth

--- run.py
+++ run.py
@@ -8,4 +8,5 @@ Copyright (c) 2019 - present AppSeed.us
 from app import app
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=5000,debug=True)
+    app.run(ssl_context="adhoc",host='0.0.0.0',port=5000,debug=True)
+    #app.run(host='0.0.0.0',port=5000,debug=True)
