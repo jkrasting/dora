bcf90a8a4fc2e36bd201c66c672ac5ecfab2bcb1
John Krasting <John.Krasting@noaa.gov>
2021-01-01 23:10:27 -0500

    Using MySQL database for user info

--- app/db.py
+++ app/db.py
@@ -1,17 +1,30 @@
-import sqlite3
+import pymysql
+import os
 
-import click
-from flask import current_app, g
+from flask import current_app
+from flask import g
 from flask.cli import with_appcontext
 
 def get_db():
     if "db" not in g:
-        g.db = sqlite3.connect(
-            "sqlite.db", detect_types=sqlite3.PARSE_DECLTYPES
+        g.db = pymysql.connect(
+            host=os.environ["DB_HOSTNAME"],
+            user=os.environ["DB_USERNAME"],
+            password=os.environ["DB_PASSWORD"],
+            db=os.environ["DB_DATABASE"],
+            cursorclass=pymysql.cursors.DictCursor,
         )
-        g.db.row_factory = sqlite3.Row
 
     return g.db
+
+#def get_db():
+#    if "db" not in g:
+#        g.db = sqlite3.connect(
+#            "sqlite.db", detect_types=sqlite3.PARSE_DECLTYPES
+#        )
+#        g.db.row_factory = sqlite3.Row
+#
+#    return g.db
 
 def close_db(e=None):
     db = g.pop("db", None)
@@ -19,18 +32,18 @@ def close_db(e=None):
     if db is not None:
         db.close()
 
-def init_db():
-    db = get_db()
+#def init_db():
+#    db = get_db()
+#
+#    with current_app.open_resource("schema.sql") as f:
+#        db.executescript(f.read().decode("utf8"))
 
-    with current_app.open_resource("schema.sql") as f:
-        db.executescript(f.read().decode("utf8"))
-
-@click.command("init-db")
-@with_appcontext
-def init_db_command():
-    init_db()
-    click.echo("Initialized the database")
+#@click.command("init-db")
+#@with_appcontext
+#def init_db_command():
+#    init_db()
+#    click.echo("Initialized the database")
 
 def init_app(app):
     app.teardown_appcontext(close_db)
-    app.click.add_command(init_db_command)
+    #app.click.add_command(init_db_command)
