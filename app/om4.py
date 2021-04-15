88d3cca81f9ee9bdc17c9bfd2cec1f5de26aaa7f
John Krasting <John.Krasting@noaa.gov>
2021-04-15 10:43:07 -0400

    Updates to OM4Labs driver script

--- app/om4.py
+++ app/om4.py
@@ -14,20 +14,53 @@ import base64
 import om4labs
 
 
-def jsonify_dirtree(dirpath):
-    """Function to return json of directory tree"""
+def jsonify_dirtree(dirpath, pptype="av"):
+    """Returns a post-processing directory tree
+
+    Parameters
+    ----------
+    dirpath : str, path-like
+        Path to top-level "pp" directory
+    pptype : str, optional
+        Type of post-processed files to return
+        either "av" or "ts", by default "av"
+
+    Returns
+    -------
+    dict
+        Dictionary of files and options to pass to file browser
+    """
 
     filelist = []
-    for component in os.scandir(dirpath):
-        if component.name.startswith("ocean") and component.is_dir():
-            for pptype in os.scandir(component):
-                if pptype.name.startswith("av"):
-                    for timechunk in os.scandir(pptype):
-                        if timechunk.is_dir():
-                            for filename in os.scandir(timechunk):
-                                filelist.append(
-                                    f"{component.name}/{pptype.name}/{timechunk.name}/{filename.name}"
-                                )
+    if pptype == "av":
+        for component in os.scandir(dirpath):
+            if (
+                component.name.startswith("ocean") or component.name.startswith("ice")
+            ) and component.is_dir():
+                for pptype in os.scandir(component):
+                    if pptype.name.startswith("av"):
+                        for timechunk in os.scandir(pptype):
+                            if timechunk.is_dir():
+                                for filename in os.scandir(timechunk):
+                                    filelist.append(
+                                        f"{component.name}/{pptype.name}/{timechunk.name}/{filename.name}"
+                                    )
+
+    elif pptype == "ts":
+        for component in os.scandir(dirpath):
+            if (
+                component.name.startswith("ocean") or component.name.startswith("ice")
+            ) and component.is_dir():
+                for pptype in os.scandir(component):
+                    if pptype.name.startswith("ts"):
+                        for freq in os.scandir(pptype):
+                            if freq.is_dir():
+                                for timechunk in os.scandir(freq):
+                                    if timechunk.is_dir():
+                                        for filename in os.scandir(timechunk):
+                                            filelist.append(
+                                                f"{component.name}/{pptype.name}/{freq.name}/{timechunk.name}/{filename.name}"
+                                            )
 
     filelist = sorted(filelist)
     filesplit = sorted([x.split("/") for x in filelist])
@@ -60,6 +93,18 @@ def jsonify_dirtree(dirpath):
 
 
 def base64it(imgbuf):
+    """ Converts in-memory PNG image to inlined ASCII format
+
+    Parameters
+    ----------
+    imgbuf : io.BytesIO
+        Binary memory buffer cotaining PNG image
+
+    Returns
+    -------
+    str
+        Base64 representation of the image
+    """
     imgbuf.seek(0)
     uri = "data:image/png;base64," + base64.b64encode(imgbuf.getvalue()).decode(
         "utf-8"
@@ -67,21 +112,27 @@ def base64it(imgbuf):
     return uri
 
 
-@app.route("/testmenu")
-def browser():
-    pathpp = "/Users/krasting/doratest/archive/Raphael.Dussin/FMS2019.01.03_devgfdl_20201120/CM4_piControl_c192_OM4p125_MZtuning/gfdl.ncrc4-intel18-prod-openmp/pp"
-    jsondir = jsonify_dirtree(pathpp)
-    return render_template("file-browser.html", pathpp=pathpp, jsondir=jsondir)
-
-
 @app.route("/analysis/om4labs", methods=["GET"])
 def om4labs_start():
+    """Flask route for calling OM4Labs
+
+    Returns
+    -------
+    Jinja template name and variables for rendering
+    """
+
+    # Get the experiment ID number from the URL or provide user with 
+    # a form to enter a post-processing path directly
     idnum = request.args.get("id")
     if idnum is None:
         return render_template("om4labs-splash.html")
 
+    # Fetch an Experiment object that houses all of the
+    # experiment metadata
     experiment = Experiment(idnum)
 
+    # Get the requested analysis from the URL or provide user with
+    # a menu of available diagnostics in OM4Labs
     analysis = request.args.get("analysis")
     if analysis is None:
         avail_diags = [x for x in dir(om4labs.diags) if not x.startswith("__")]
@@ -96,28 +147,57 @@ def om4labs_start():
             "om4labs-start.html", avail_diags=avail_diags, idnum=idnum
         )
 
+    # Get the list of files to analyse from the URL or provide user with
+    # a clickable menu to browse for files to analyse
     files = request.args.get("files")
-    if files is None:
-        jsondir = jsonify_dirtree(experiment.pathPP)
-        return render_template(
-            "file-browser.html", jsondir=jsondir, analysis=analysis, idnum=idnum
-        )
-    if len(files) == 0:
-        jsondir = jsonify_dirtree(experiment.pathPP)
-        return render_template(
-            "file-browser.html", jsondir=jsondir, analysis=analysis, idnum=idnum
-        )
-    files = files.split(",")
-    files = [f"{experiment.pathPP}/{x}" for x in files if not x.startswith("j1")]
 
+    # The "section transports" diagnostic accepts a top-level pp dir
+    if analysis not in ["section_transports"]:
+        files = "" if files is None else files
+        if len(files) == 0:
+            jsondir = jsonify_dirtree(experiment.pathPP)
+            return render_template(
+                "file-browser.html", jsondir=jsondir, analysis=analysis, idnum=idnum
+            )
+        files = files.split(",")
+    else:
+        files = []
+        
+    # infer static file from frepp structure
+    if len(files) > 0:
+        static = files[0].split("/")[0]
+        static = f"{experiment.pathPP}/{static}/{static}.static.nc"
+        files = [f"{experiment.pathPP}/{x}" for x in files if not x.startswith("j1")]
+    else:
+        static = None
+
+    # Intialize and empty dictionary of options pertaining to the
+    # requested diagnostic
     dict_args = om4labs.diags.__dict__[analysis].parse(template=True)
-    dict_args["platform"] = "gfdl"
+
+    # Tell OM4Labs we want streaming image buffers back
     dict_args["format"] = "stream"
+
+    # Populate various options for running the diagnostic from
+    # the experiment metadata
+    dict_args["label"] = experiment.expName
+    dict_args["ppdir"] = [experiment.pathPP]
+
+    # Tell OM4Labs where to find the observational data
+    dict_args["platform"] = os.environ["OM4LABS_PLATFORM"]
+
+    # Pass in the paths to the selected files and the inferred
+    # static / topog file
     dict_args["infile"] = files
-    # dict_args["obsfile"] = "/Users/krasting/pkgs/om4labs/testing/test_data/obs/WOA13_ptemp+salinity_annual_35levels.nc"
+    dict_args["static"] = static
+    dict_args["topog"] = static
+
+    # *** Run OM4Labs ***
     imgbufs = om4labs.diags.__dict__[analysis].run(dict_args)
+
+    # Convert image buffers to in-lined images
     figures = [base64it(x) for x in imgbufs]
-    print(files)
+
     return render_template(
         "om4labs-results.html", figures=figures, experiment=experiment
     )
