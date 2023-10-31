1f2250f2e251ab2e3ff79f9e7e6d28cade135edc
John Krasting <John.Krasting@noaa.gov>
2023-10-31 16:15:01 -0400

    Moved order of dora import
    - The environment must now be defined before importing
      the dora module

--- run.py
+++ run.py
@@ -5,8 +5,8 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
+import os
 import sys
-from dora import dora
 from dotenv import load_dotenv
 from OpenSSL import SSL
 
@@ -17,9 +17,13 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         if sys.argv[1] == "dev":
             load_dotenv(".devenv")
+            print("We are here")
+            print(os.environ.keys())
+            from dora import dora
             dora.run(host=HOST, port=PORT, debug=True, ssl_context="adhoc")
         elif sys.argv[1] == "nossl":
             load_dotenv(".devenv")
+            from dora import dora
             dora.run(host=HOST, port=PORT, debug=True)
         elif sys.argv[1] == "localcert":
             load_dotenv(".devenv")
@@ -27,6 +31,8 @@ if __name__ == "__main__":
             context.use_privatekey_file("certs/key.pem")
             context.use_certificate_file("certs/cert.pem")
             context = ("certs/cert.pem", "certs/key.pem")
+            from dora import dora
             dora.run(host=HOST, port=PORT, debug=True, ssl_context=context)
     else:
+        from dora import dora
         dora.run(ssl_context="adhoc", host=HOST, port=PORT, debug=True)
